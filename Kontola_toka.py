62dc88fff14554f02b1e6f43d762498ee4b320a7
Djordje <djola92@gmail.com>
2022-12-03 14:04:53 +0100

    fajlovi sa casa

---
+++ Kontola_toka.py
@@ -0,0 +1,31 @@
+
+
+
+
+# 1 - prikaz ; 2 - kupovina ; 3 - izlaz
+
+
+proizvod = "duks"
+cena = 3000
+
+# komanda = int(input("Unesite komandu: ")) 
+
+
+# if komanda == 1:
+#     print("Prikazi proizvode")
+#     print("Proizvod:" , proizvod, "Cena:" , cena)
+# if komanda == 2:
+#     print("Kupovina...")
+#     stanje = int(input("Unesite stanje na racunu: "))
+#     if stanje >= cena:
+#         print("Uspesna kupovina!")
+#     if stanje < cena:
+#         print("Neuspesna kupovina...")
+# if komanda == 3:
+#     print("Izlaz")
+
+broj = 5
+
+
+
+    
